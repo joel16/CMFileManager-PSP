82ceb6a4e9829db396d7f7cd76322937bdd96376
Joel16 <sauskethunderkid@gmail.com>
2018-11-12 01:12:50 -0600

    Deploy builds on gh-pages

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -5,3 +5,5 @@ if [ "$BUILD_PSP" = "YES" ]; then
     echo psp-config = `psp-config --psp-prefix`
     make -j 4
 fi
+
+rm -rf .git common include libs src .gitattributes .gitignore CMFileManager.elf CMFileManager.prx ICON0.png LICENSE Makefile PARAM.SFO
