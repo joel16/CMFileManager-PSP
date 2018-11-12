88cb13335f2993d93c99c183ddf4bc84a0836412
Joel16 <sauskethunderkid@gmail.com>
2018-11-12 01:33:33 -0600

    Remove oslib dir after we're done with it

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 
-git clone https://github.com/dogo/oslibmodv2.git
-cd oslibmodv2/
-make clean && make && make install install
-
 if [ "$BUILD_PSP" = "YES" ]; then
-    echo PSPDEV = $PSPDEV
-    echo psp-config = `psp-config --psp-prefix`
-    make -j 4
+	echo PSPDEV = $PSPDEV
+	echo psp-config = `psp-config --psp-prefix`
+	git clone https://github.com/dogo/oslibmodv2.git
+	cd oslibmodv2/
+	make clean && make && make install install
+	cd ../
+	rm -rf oslibmodv2/
+	make -j 4
 fi
 
 rm -rf .git common include libs src .gitattributes .gitignore CMFileManager.elf CMFileManager.prx ICON0.png LICENSE Makefile PARAM.SFO
