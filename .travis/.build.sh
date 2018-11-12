7e5c440cdc30e44b3c1b2d15a9acb24a53eda146
Joel16 <sauskethunderkid@gmail.com>
2018-11-12 01:30:52 -0600

    Fix bash script syntax

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -2,9 +2,9 @@
 
 git clone https://github.com/dogo/oslibmodv2.git
 cd oslibmodv2/
-make clean && make && make install
+make clean && make && make install install
 
-if ["$BUILD_PSP" = "YES"]; then
+if [ "$BUILD_PSP" = "YES" ]; then
     echo PSPDEV = $PSPDEV
     echo psp-config = `psp-config --psp-prefix`
     make -j 4
