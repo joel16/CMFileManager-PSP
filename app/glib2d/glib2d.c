fd4e03123aefb80bb09fc34cab7150fe681ee2ef
Joel16 <sauskethunderkid@gmail.com>
2019-03-28 23:22:53 -0500

    Include missing exports

--- app/glib2d/glib2d.c
+++ app/glib2d/glib2d.c
@@ -61,11 +61,6 @@
 #define OBJ_I                   rctx.obj[i]
 #define TRANSFORM               tstack[tstack_size - 1]
 
-/// Checks whether a result code indicates success.
-#define R_SUCCEEDED(res)   ((res) >= 0)
-/// Checks whether a result code indicates failure.
-#define R_FAILED(res)      ((res) < 0)
-
 /* Enumerations */
 
 typedef enum {
