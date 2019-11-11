5a966b94dc66bfc9bc0678624ffc03b523c6f0d9
Joel16 <joel16@iastate.edu>
2019-11-10 22:49:38 -0500

    Remove Makefile.psp before deployment

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-rm -rf .git .travis common libs .gitattributes .gitignore .travis.yml ICON0.PNG LICENSE Makefile README.md
+rm -rf .git .travis common libs .gitattributes .gitignore .travis.yml ICON0.PNG LICENSE Makefile.psp README.md
 mkdir -p PSP/GAME/CMFileManager
 mv app/EBOOT.PBP PSP/GAME/CMFileManager/APP.PBP && mv launcher/EBOOT.PBP PSP/GAME/CMFileManager/EBOOT.PBP
 mv audio_driver/audio_driver.prx PSP/GAME/CMFileManager/audio_driver.prx
