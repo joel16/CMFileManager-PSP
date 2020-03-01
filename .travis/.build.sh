df422286f47bd0e7cf7322badf80e24419e046fd
joel16 <joel16@iastate.edu>
2020-03-01 10:02:42 -0500

    Make changes to travis to include fs_driver.prx

--- .travis/.build.sh
+++ .travis/.build.sh
@@ -5,5 +5,6 @@ mkdir -p PSP/GAME/CMFileManager
 mv app/EBOOT.PBP PSP/GAME/CMFileManager/APP.PBP && mv launcher/EBOOT.PBP PSP/GAME/CMFileManager/EBOOT.PBP
 mv audio_driver/audio_driver.prx PSP/GAME/CMFileManager/audio_driver.prx
 mv display_driver/display_driver.prx PSP/GAME/CMFileManager/display_driver.prx
+mv fs_driver/fs_driver.prx PSP/GAME/CMFileManager/fs_driver.prx
 zip -r CMFileManager-PSP.zip PSP/
-rm -rf app audio_driver display_driver launcher PSP
+rm -rf app audio_driver display_driver fs_driver launcher PSP
