d7a80a9d96fabd456c5fd1492913f93b5afc15cc
Joel16 <joel16@iastate.edu>
2020-05-07 13:51:52 -0400

    Use externfor texture declaration

--- app/include/textures.h
+++ app/include/textures.h
@@ -3,9 +3,9 @@
 
 #include <glib2d.h>
 
-g2dTexture *icon_app, *icon_archive, *icon_audio, *icon_cd, *icon_dir, *icon_dir_dark, *icon_file, *icon_image, *icon_prx, *icon_text, \
-          *icon_check, *icon_check_dark, *icon_uncheck, *icon_uncheck_dark, *icon_toggle_on, *icon_toggle_dark_on, *icon_toggle_off, \
-          *icon_radio_off, *icon_radio_on, *icon_radio_dark_off, *icon_radio_dark_on, *icon_nav_drawer, *icon_back, \
+extern g2dTexture *icon_app, *icon_archive, *icon_audio, *icon_cd, *icon_dir, *icon_dir_dark, *icon_file, *icon_image, *icon_prx, \
+          *icon_text, *icon_check, *icon_check_dark, *icon_uncheck, *icon_uncheck_dark, *icon_toggle_on, *icon_toggle_dark_on, \
+          *icon_toggle_off, *icon_radio_off, *icon_radio_on, *icon_radio_dark_off, *icon_radio_dark_on, *icon_nav_drawer, *icon_back, \
           *options_dialog, *options_dialog_dark, *properties_dialog, *properties_dialog_dark, *dialog, *dialog_dark, \
           *battery_20, *battery_20_charging, *battery_30, *battery_30_charging, *battery_50, *battery_50_charging, \
           *battery_60, *battery_60_charging, *battery_80, *battery_80_charging, *battery_90, *battery_90_charging, \
--- app/source/textures.c
+++ app/source/textures.c
@@ -33,6 +33,17 @@ extern unsigned int ic_fso_type_app_png_size, ic_fso_type_compress_png_size, ic_
 	btn_playback_repeat_overlay_png_size, btn_playback_shuffle_overlay_png_size, bg_header_png_size, ic_material_light_sdcard_png_size, ic_material_light_secure_png_size, 
 	ic_material_light_sdcard_dark_png_size, ic_material_light_secure_dark_png_size, ic_play_btn_png_size;
 
+g2dTexture *icon_app, *icon_archive, *icon_audio, *icon_cd, *icon_dir, *icon_dir_dark, *icon_file, *icon_image, *icon_prx, *icon_text, \
+          *icon_check, *icon_check_dark, *icon_uncheck, *icon_uncheck_dark, *icon_toggle_on, *icon_toggle_dark_on, *icon_toggle_off, \
+          *icon_radio_off, *icon_radio_on, *icon_radio_dark_off, *icon_radio_dark_on, *icon_nav_drawer, *icon_back, \
+          *options_dialog, *options_dialog_dark, *properties_dialog, *properties_dialog_dark, *dialog, *dialog_dark, \
+          *battery_20, *battery_20_charging, *battery_30, *battery_30_charging, *battery_50, *battery_50_charging, \
+          *battery_60, *battery_60_charging, *battery_80, *battery_80_charging, *battery_90, *battery_90_charging, \
+          *battery_full, *battery_full_charging, *battery_low, *battery_unknown, *wifi_off, *wifi_on, *usb_icon, \
+          *default_artwork, *default_artwork_blur, *btn_play, *btn_pause, *btn_rewind, *btn_forward, \
+          *btn_repeat, *btn_shuffle, *btn_repeat_overlay, *btn_shuffle_overlay, \
+          *bg_header, *icon_sd, *icon_secure, *icon_sd_dark, *icon_secure_dark, *ic_play_btn;
+
 void Textures_Load(void) {
 	icon_app = g2dTexLoadMemory(ic_fso_type_app_png_start, ic_fso_type_app_png_size, G2D_SWIZZLE);
 	icon_archive = g2dTexLoadMemory(ic_fso_type_compress_png_start, ic_fso_type_compress_png_size, G2D_SWIZZLE);
