00c847e0bcbb8c4537ffd3273cd0cbf616299077
Joel16 <joel16dev@gmail.com>
2021-12-20 01:07:35 -0500

    mp3: Fix some cover arts not being loaded

--- app/source/audio/mp3.cpp
+++ app/source/audio/mp3.cpp
@@ -11,74 +11,84 @@ namespace MP3 {
     static u64 frames_read = 0, total_samples = 0;
     static int channels = 0;
     static long sample_rate = 0;
-
-    static inline void ReadID3v1Field(const char* field, size_t length, std::string& s) {
-        char buf[length + 1];
-        
-        // copy the data into a temporary buffer to add a zero byte at the end of the text
-        std::memcpy(buf, field, length);
-        buf[length] = 0;
-        s = buf;
-    }
-
-    static void ProcessID3v1(AudioMetadata& info, const mpg123_id3v1& id3) {
-        std::string temp;
-
-        if (info.title.empty()) {
-            MP3::ReadID3v1Field(id3.title, 30, temp);
-            info.title = temp;
-        }
-
-        if (info.album.empty()) {
-            MP3::ReadID3v1Field(id3.album, 30, temp);
-            info.album = temp;
-        }
-        
-        if (info.artist.empty()) {
-            MP3::ReadID3v1Field(id3.artist, 30, temp);
-            info.artist = temp;
-        }
-
-        if (info.year.empty()) {
-            MP3::ReadID3v1Field(id3.year, 30, temp);
-            info.year = temp;
-        }
-    }
-
-    void ProcessID3v2(AudioMetadata& info, const mpg123_id3v2& id3) {
-        if (id3.title && id3.title->p)
-            info.title = id3.title->p;
-
-        if (id3.album && id3.album->p)
-            info.album = id3.album->p;
-
-        if (id3.artist && id3.artist->p)
-            info.artist = id3.artist->p;
-
-        if (id3.genre && id3.genre->p)
-            info.genre = id3.genre->p;
-
-        if (id3.year && id3.year->p)
-            info.year = id3.year->p;
-
-        size_t i = 0;
-        for (i = 0; i < id3.pictures; ++i) {
-            const mpg123_picture& pic = id3.picture[i];
-            printf("\ncount: %d, pic.type: %d, pic.mime_type.p: %s\n", i, pic.type, pic.mime_type.p);
+    
+    /* Helper for v1 printing, get these strings their zero byte. */
+    void SafePrint(std::string &data, char *value, size_t size) {
+        char safe[31];
+        if (size > 30)
+            return;
             
-            if ((pic.type == mpg123_id3_pic_front_cover) || (pic.type == mpg123_id3_pic_back_cover)) {
-                if ((!strcasecmp(pic.mime_type.p, "image/jpg")) || (!strcasecmp(pic.mime_type.p, "image/jpeg"))) {
-                    metadata.cover_image = Textures::LoadImageBufferJPEG(pic.data, pic.size);
-                    break;
+        std::memcpy(safe, value, size);
+        safe[size] = 0;
+        data = std::string(safe);
+    }
+    
+    /* Print out ID3v1 info. */
+    void ProcessID3v1(AudioMetadata &tag, mpg123_id3v1 *v1) {
+        MP3::SafePrint(tag.title, v1->title, sizeof(v1->title));
+        MP3::SafePrint(tag.artist, v1->artist, sizeof(v1->artist));
+        MP3::SafePrint(tag.album, v1->album, sizeof(v1->album));
+        MP3::SafePrint(tag.year, v1->year, sizeof(v1->year));
+        MP3::SafePrint(tag.comment, v1->comment, sizeof(v1->comment));
+    }
+    
+    /* Split up a number of lines separated by \n, \r, both or just zero byte
+    and print out each line. */
+    void PrintLines(std::string &data, mpg123_string *inlines) {
+        int hadcr = 0, hadlf = 0;
+        char *lines = nullptr;
+        char *line = nullptr;
+        size_t len = 0;
+        
+        if (inlines != nullptr && inlines->fill) {
+            lines = inlines->p;
+            len = inlines->fill;
+        }
+        else
+            return;
+            
+        line = lines;
+        for(size_t i = 0; i < len; ++i) {
+            if (lines[i] == '\n' || lines[i] == '\r' || lines[i] == 0) {
+                char save = lines[i]; /* saving, changing, restoring a byte in the data */
+                
+                if (save == '\n')
+                    ++hadlf;
+                
+                if (save == '\r') 
+                    ++hadcr;
+                
+                if ((hadcr || hadlf) && hadlf % 2 == 0 && hadcr % 2 == 0)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+                    line = "";
+#pragma GCC diagnostic pop 
+                    
+                if (line) {
+                    lines[i] = 0;
+                    data = line;
+                    line = nullptr;
+                    lines[i] = save;
                 }
-                else if (!strcasecmp(pic.mime_type.p, "image/png")) {
-                    metadata.cover_image = Textures::LoadImageBufferPNG(pic.data, pic.size);
-                    break;
-                }
+            }
+            else {
+                hadlf = hadcr = 0;
+                if (line == nullptr)
+                    line = lines + i;
             }
         }
     }
     
+    /* Print out the named ID3v2  fields. */
+    void ProcessID3v2(AudioMetadata &tag, mpg123_id3v2 *v2) {
+        MP3::PrintLines(tag.title, v2->title);
+        MP3::PrintLines(tag.artist, v2->artist);
+        MP3::PrintLines(tag.album, v2->album);
+        MP3::PrintLines(tag.year, v2->year);
+        MP3::PrintLines(tag.comment, v2->comment);
+        MP3::PrintLines(tag.genre, v2->genre);
+    }
+
     int Init(const std::string &path) {
         int error = 0;
 
@@ -116,9 +126,30 @@ namespace MP3 {
         mpg123_id3v2 *id3v2;
         if (metadata.has_meta & MPG123_ID3 && mpg123_id3(mp3, &id3v1, &id3v2) == MPG123_OK) {
             if (id3v1)
-                MP3::ProcessID3v1(metadata, *id3v1);
+                MP3::ProcessID3v1(metadata, id3v1);
             if (id3v2)
-                MP3::ProcessID3v2(metadata, *id3v2);
+                MP3::ProcessID3v2(metadata, id3v2);
+                
+            for (size_t i = 0; i < id3v2->pictures; ++i) {
+                mpg123_picture *pic = &id3v2->picture[i];
+                
+                // Front cover or other
+                if ((pic->type == 3)) {
+                    if ((!strcasecmp(pic->mime_type.p, "image/jpeg")) || (!strcasecmp(pic->mime_type.p, "image/jpg"))) {
+                        metadata.cover_image = Textures::LoadImageBufferJPEG(pic->data, pic->size);
+                        break;
+                    }
+                    else if (!strcasecmp(pic->mime_type.p, "image/png")) {
+                        metadata.cover_image = Textures::LoadImageBufferPNG(pic->data, pic->size);
+                        
+                        // I have trust issues
+                        if (!metadata.cover_image)
+                            metadata.cover_image = Textures::LoadImageBufferJPEG(pic->data, pic->size);
+                        
+                        break;
+                    }
+                }
+            }
         }
         
         mpg123_getformat(mp3, &sample_rate, &channels, nullptr);
